6016d39690540153e83ab2142e544dfad281f66b
Axel Iván Solano González <pandaxel117@gmail.com>
2024-04-25 07:40:05 -0600

    Initial Commit

---
+++ AxelDex/AxelDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  AxelDexApp.swift
+//  AxelDex
+//
+//  Created by Axel Iván Solano González on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct AxelDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
